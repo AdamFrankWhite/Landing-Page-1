3176b0143a3792ce959ada271459f4e1baf3b0b3
Adam Frank White <adamfrankwhite@gmail.com>
2019-07-18 00:25:08 +0100

    finished reviews layout, added favicon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Merienda|Merriweather" rel="stylesheet">
+		<link rel="shortcut icon" type="image/x-icon" href="icons/favicon.ico" />
 		<link rel="stylesheet" href="styles.css">
 		<title>Quitting Weed: The Complete Guide</title>
 	</head>
@@ -21,6 +22,7 @@
 				      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
 				      <a class="nav-item nav-link" href="#features">Features</a>
 				      <a class="nav-item nav-link" href="#samples">Samples</a>
+							<a class="nav-item nav-link" href="#chapters">Chapters</a>
 				      <a class="nav-item nav-link" href="#testimonials">Testimonials</a>
 				    </div>
 				  </div>
@@ -44,7 +46,7 @@
 				</div>
 			</div>
 
-			<section class="features">
+			<section id="features" class="features">
 				<div class="text-center">
 					<h2 class="pt-4 mb-0">Book Features</h2>
 					<h6 class="text-muted">what you will learn</h6>
@@ -111,8 +113,10 @@
 					</div>
 				</div>
 			</section>
-			<section class="testimonials">
-				<div class="text-center">
+
+			<!-- Testimonials -->
+			<section id="testimonials" class="testimonials">
+				<div class="heading text-center">
 					<h2 class="pt-4 mb-0">Reviews</h2>
 					<h6 class="text-muted">read what others think</h6>
 					<hr class="reviews-rule">
@@ -120,18 +124,180 @@
 
 				<div class="review">
 					<div class="review-cont">
-						<h2>Kathleen B.</h2>
-						<img src="icons/quote-open.png">
-						<cite>
-							<p>This book from the first few pages made me feel I wasn't alone.</p>
-							<p>Highly recommended by someone who thought they'd never escape the tyranny of the weed.</p>
-						</cite>
-						<img src="icons/quote-end.png">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p>This book from the first few pages made me feel I wasn't alone.</p>
+								<p class="mb-0">Highly recommended by someone who thought they'd never escape the tyranny of the weed.</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">Kathleen B.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p>This book covers everything. Unfortunately this area of addiction is often overlooked or downplayed. I learned some things I didn't know.</p>
+								<p>There's no judgment here, just a straightforward guide to understanding what is happening with your body and brain on this drug. If you don't have a problem with weed, or it is even beneficial to you that is totally cool, but that is not the case for some.</p>
+								<p class="mb-0">Sadly for myself and others it has become a toxic influence. There is no shame in becoming addicted to an addictive substance, if this happens to resonate with you ( I mean you are looking into a book specifically about quitting weed and reading reviews for it 😅) I definitely recommend it, as it will walk you through the process of understanding why this stuff is causing problems for you, what to expect when you quit, and how to recognize, and even hopefully prevent an oncoming relapse.</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">Mimi</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p class="mb-0">I’ve been thinking about quitting weed for a few months, and this book really opened up my eyes to how important it is that I actually do it. It talks about the science behind the addiction and provides honest and humbling thoughts/phrases to remember during your journey to sobriety to help you stay on track (which was always the hardest part for me).</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">Michelle M.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p class="mb-0">An excellent, well-organized, and clearly written manual for quitting marijuana addiction. If you or a loved one has this problem, this book is an excellent place to start.</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">Glenn V.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p class="mb-0">I am a child and adolescent psychologist and this book is the best resource I have found to address marajuana addiction. I see so many unhappy, underachieving youth who are trapped in a terrible cyle with this substance. The author clearly explains the problems chronic users face while weaving in research and resources. He also provides a lot of encouagement for those toying with quitting. I will recommend this book to any family I meet who is dealing with this challenge.</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">Katherine Q.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<div class="quote">
+							<img src="icons/quote-open.png">
+							<cite>
+								<p class="mb-0">This book is right on point. Clearly the author knows what he’s talking about. Good for anyone at any point of their journey. Learned even more reasons why I needed to do this. Easy to read.</p>
+							</cite>
+							<img src="icons/quote-end.png">
+							<p class="reviewer">SM</p>
+						</div>
 					</div>
 				</div>
 
 			</section>
-			<img src="booksAll.png">
+
+			<section id="chapters" class="chapters">
+				<div class="text-center">
+					<h2 class="pt-4 mb-0">Chapter Overviews</h2>
+					<h6 class="text-muted">book contents</h6>
+					<hr class="feat-rule">
+					<div class="chapters-group">
+						<div class="card">
+						  <img class="card-img-top" src="images/chains.jpg" alt="chains">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 1:</h5><h5>The Invisible Chains</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 2:</h5><h5>Analysing Your Habit</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 3:</h5><h5>The Science of Cannabis Addiction</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 4:</h5><h5>The Psychology of Addiction</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 5:</h5><h5>The Quitting Process</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 6:</h5><h5>Quitting Advice and Strategies</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 7:</h5><h5>Relapses, Obstacles and Mental Gymnastics</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 8:</h5><h5>The Quitting Mindset</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						    <h5 class="card-title">Chapter 9:</h5><h5>The Motivation Chapter</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+						<div class="card">
+						  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+						  <div class="card-body">
+						   	<h5 class="card-title">Appendix:</h5><h5>Recovery Experiences</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<div class="text-center">
+				<img src="booksAll.png">
+			</div>
 		</main>
 		<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
     crossorigin="anonymous"></script>
