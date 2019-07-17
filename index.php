cc820a6e46576b2b38bdbb5b315ff6cc53f54424
Adam Frank White <adamfrankwhite@gmail.com>
2019-07-17 12:32:11 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Merienda|Merriweather" rel="stylesheet">
+		<link rel="stylesheet" href="styles.css">
+		<title>Quitting Weed: The Complete Guide</title>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-info">
+				<div id="mainMenu" class="mx-auto">
+				  <a class="navbar-brand" href="#">Quitting Weed: The Complete Guide</a>
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				    <span class="navbar-toggler-icon"></span>
+				  </button>
+				  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				    <div class="navbar-nav">
+				      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+				      <a class="nav-item nav-link" href="#features">Features</a>
+				      <a class="nav-item nav-link" href="#samples">Samples</a>
+				      <a class="nav-item nav-link" href="#testimonials">Testimonials</a>
+				    </div>
+				  </div>
+				</div>
+			</nav>
+
+		</header>
+		<main>
+
+			<div class="jumbotron">
+			  <div class="jumbo-content">
+					<img src="ebookMini.png">
+					<h1 class="display-4">Is it time you left cannabis behind?</h1>
+
+				  <p class="lead">Are you struggling to overcome your marijuana addiction? It's okay. It's not as easy as everyone makes out.</p>
+				  <hr class="my-4">
+
+				  <p>Begin your journey towards sobriety today</p>
+				  <a class="btn btn-primary btn-lg" href="#" role="button">Purchase</a>
+					<a class="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+				</div>
+			</div>
+
+			<section class="features">
+				<div class="text-center">
+					<h2 class="pt-4 mb-0">Book Features</h2>
+					<h6 class="text-muted">what you will learn</h6>
+					<hr class="feat-rule">
+					<div class="card-group">
+						<div class="card">
+							<img src="icons/book.svg">
+	  					<div class="card-body">
+						    <h5 class="card-title">The Science of Addiction</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Understand the science and psychology that drives your addiction</h6>
+
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/heartbeat.svg">
+	  					<div class="card-body">
+							  <h5 class="card-title">Withdrawal symptoms</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Understand cannabis withdrawal symptoms and how to best treat them</h6>
+
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/column-chart.svg">
+	  					<div class="card-body">
+						    <h5 class="card-title">Analyse</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Learn to view your habit and the drug in a different light, through reflecting on your use</h6>
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/puzzle.svg">
+	  					<div class="card-body">
+								<h5 class="card-title">Obstacles</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Understand how to overcome the obstacles to sobriety – mental gymnastics, social pressure, boredom, nostalgia</h6>
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/leaf.svg">
+	  					<div class="card-body">
+						    <h5 class="card-title">Be Aware</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Know what to expect during the quitting process – relapses, self-doubt, emotional ups and downs</h6>
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/smiling-line.svg">
+	  					<div class="card-body">
+								<h5 class="card-title">Mindset</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Discover the knowledge and approaches necessary for maintaining long-term sobriety</h6>
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/book.svg">
+	  					<div class="card-body">
+						    <h5 class="card-title">Motivation</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Have a selection of cannabis-specific motivational quotes and reflections to consult when struggling</h6>
+						  </div>
+						</div>
+						<div class="card">
+							<img src="icons/book.svg">
+	  					<div class="card-body">
+						    <h5 class="card-title">Prepare</h5>
+						    <h6 class="card-subtitle mb-2 text-muted">Learn strategies and tips for maximising your chances of success</h6>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="testimonials">
+				<div class="text-center">
+					<h2 class="pt-4 mb-0">Reviews</h2>
+					<h6 class="text-muted">read what others think</h6>
+					<hr class="reviews-rule">
+				</div>
+
+				<div class="review">
+					<div class="review-cont">
+						<h2>Kathleen B.</h2>
+						<img src="icons/quote-open.png">
+						<cite>
+							<p>This book from the first few pages made me feel I wasn't alone.</p>
+							<p>Highly recommended by someone who thought they'd never escape the tyranny of the weed.</p>
+						</cite>
+						<img src="icons/quote-end.png">
+					</div>
+				</div>
+
+			</section>
+			<img src="booksAll.png">
+		</main>
+		<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+    crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="/script.js"></script>
+  </body>
+</html>
