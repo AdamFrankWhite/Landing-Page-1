de9f1a9e3ebd16b6ba989f283aa2757ec9e8d124
Adam Frank White <adamfrankwhite@gmail.com>
2019-07-27 19:20:41 +0100

    added share buttons to scroll feature

--- script.js
+++ script.js
@@ -18,6 +18,36 @@ for (let i=1; i<chapters.length+1; i++) {
 
 function previewOpen(chap) {
   $(chap).toggle({"display": "block"})
+  // $(chap).prev().children().toggleText('˄', '˅')
+  $(chap).prev().children().toggleText(chevronChange,chevronChange)
 
 
 }
+function chevronChange() {
+  if ($(this).text()=="˄") {
+    return "˅"
+  } else {
+    return "˄"
+  }
+}
+
+$(document).ready(function() {
+    // Get the current top location of the nav bar.
+    var stickyTop = $('#sticky').offset().top;
+
+    // Set the header's height to its current height in CSS
+    // If we don't do this, the content will jump suddenly when passing through stickyNavTop.
+    $('header').height($('header').height());
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= stickyTop+300) {
+            $('#sticky').addClass('fixed-header');
+            $('#sticky').removeClass('sticky');
+            $('#share').css({"display": "inline"}).addClass('sidebar')
+
+        } else {
+            $('#sticky').removeClass('fixed-header');
+  
+        }
+    });
+});
